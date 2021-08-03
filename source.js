4f1117ef16868a386ff81eeee43d4e6b06d93da6
Alejandro Morales <alejandromorjim@gmail.com>
2021-08-03 13:00:44 -0500

    agregamos un saludo

--- source.js
+++ source.js
@@ -1,6 +1,9 @@
 $("#btn_first").click(function(){
     $("h1:first").html("Change");
 })
+$(document).ready(function(){
+    console.log("buen dia")
+});
 
 $("#btn_last").click(function(){
     $("h1:last").html("also change");
